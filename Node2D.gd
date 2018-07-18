84e4899e6e0383445d61dd493f60ac90152f9bd9
Galileo <galileofcap@gmail.com>
2018-07-18 11:40:17 -0300

    ahora va y viene, por dos parabolas (qedo un obalo)

--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var screen_size
+var X0 = 0
+var Y0 = 300.0
 
 func aparabola(hmax, xhmax, xcayo):
 	var r = hmax/(xhmax*xhmax - xhmax * xcayo)
@@ -13,8 +15,8 @@ func _ready():
 	screen_size = get_viewport_rect().size
   #  pad_size = get_node("left").get_texture().get_size()
 	print(screen_size)
-	position.x = 0
-	position.y = 600
+	position.x = X0
+	position.y = Y0
 
 func _process(delta):
 # En general le sumo uno a x, le sumo f'(x) a y
@@ -24,13 +26,15 @@ func _process(delta):
 # y = f(x) = ax^2 + bx + c => f'(x) = 2ax + b
 # Para calcular a y b, despejamos b en funcion de a. Y despues despejamos a "a" sabiendo valores qe qeriamos qe de
 # (El punto maximo y los ceros)
-	var hmax = 600.0
+	var hmax = 300.0
 	var xhmax = 500.0
 	var xcayo = 1000.0
 	var a = aparabola(hmax, xhmax, xcayo) #OJO, qe si no tienen decimales, redondea
 	var b = bparabola(a, xcayo)
-	# print(a, b)
-	position.x += 1
-	position.y -= (2 * a * position.x) + b
-	if position.x == xcayo:
-		position.x = 0
+	#print(position.x)
+	if position.y <= Y0:
+		position.x += 1
+		position.y -= (2 * a * position.x) + b
+	if position.y > Y0:
+		position.x += -1
+		position.y += -1 * ((2 * a * position.x) + b)
